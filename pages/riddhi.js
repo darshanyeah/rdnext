ee465d55ce3e629aacc8465f1278aafc51582c4a
Darshan Parmar <darshan.silverwing@gmail.com>
2022-04-30 16:49:51 +0530

    form added

--- pages/riddhi.js
+++ pages/riddhi.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
 
 export default function riddhi() {
-  return <div>riddhi</div>;
+  return (
+    <Container>
+      <div className="mt-3">
+        <Form.Group>
+          <Form.Label>Message</Form.Label>
+          <textarea
+            className="form-control h-50"
+            title="message"
+            name="message"
+            placeholder="message"
+          />
+        </Form.Group>
+      </div>
+      <Button className="mt-3" variant="secondary">
+        Send
+      </Button>
+    </Container>
+  );
 }
