3a06b8ff7644a1183f290b2a1f6083fae6753cef
Darshan Parmar <darshan.silverwing@gmail.com>
2022-05-16 23:15:42 +0530

    ui added - resolved conflicts

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 export const LOREM_IPSUM =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-export default function test() {
+export default function Test() {
   const typeRef = useRef(null);
 
   const [marginBottomTexts, setMarginBottomTexts] = useState(0);
